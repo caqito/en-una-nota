931617967089a701189ba00b370a7bed87c3a2ae
Nicolas Silva <nicolas.silva3582@gmail.com>
2026-08-12 17:22:50 -0300

    Actualizar canciones

--- src/songs.js
+++ src/songs.js
@@ -1,7 +1,12 @@
 export const songs = [
-    'DUKI - GIVENCHY (Video Oficial).mp3',
-    'DUKI, Jhayco - RoCKSTAR 2.0 (Video Oficial).mp3',
-    'DUKI, Ysy A, C.R.O - Hijo de la Noche (Video Oficial).mp3',
-    'Quavo Mododiablo FIX.mp3',
-    'Tumbando el Club (Remix) (Official Video) FIX.mp3',
-  ]
+  "DUKI - Cine 🚬.mp3",
+  "DUKI - Cine.mp3",
+  "DUKI - GIVENCHY (Video Oficial).mp3",
+  "DUKI - Goteo (Video Oficial).mp3",
+  "DUKI - TOP 5 (Video Oficial).mp3",
+  "DUKI, Jhayco - RoCKSTAR 2.0 (Video Oficial).mp3",
+  "DUKI, Myke Towers - Nueva Era.mp3",
+  "DUKI, Ysy A, C.R.O - Hijo de la Noche (Video Oficial).mp3",
+  "Quavo Mododiablo FIX.mp3",
+  "Tumbando el Club (Remix) (Official Video) FIX.mp3",
+]
